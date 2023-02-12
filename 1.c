69960d7f485985350377f13bdaa8e8241f41345c
Pavel Buharin <asasin8849@gmail.com>
2023-02-12 22:40:31 +0700

    первая задача

---
+++ 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int* init(int n)
+{
+int* arr = malloc(n * sizeof(int));
+int i;
+for (i = 0; i < n; ++i)
+{
+    arr[i] = i;
+}
+return arr;
+}
+
+int main()
+{
+    int n = 10;
+    int* arr = init(n);
+    int i;
+    for (i = 0; i < n; ++i)
+{
+    printf("%d\n", arr[i]);
+}
+    return 0;
+}
